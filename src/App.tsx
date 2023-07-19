c94e50cb7812ecbe6e4c28eef331c73f1b131848
hermiod <admin@hermiod.com>
2023-07-18 21:10:02 -0700

    React-query and material added to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { QueryClient, QueryClientProvider, useQuery } from '@tanstack/react-query'
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const ankiConnect = async (params: any) => {
+    const res = await fetch('http://localhost:8765', {
+        method: "POST",
+        body: JSON.stringify({
+            version: 6,
+            ...params
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+    if (!res.ok) {
+        throw new Error('Network response was not ok')
+    }
+    const data: any = await res.json();
+    if (data.error) {
+        throw new Error(data.error)
+    }
+    return data.result;
+}
+
+const fetchDecks = async (): Promise<string[]> => {
+    return ankiConnect({ action: "deckNames" });
+}
+
+
+function Home() {
+    const { data: decks } = useQuery({
+        queryFn: fetchDecks,
+        queryKey: ["decks"]
+    });
+
+    return (
+        <>
+            <div>
+                {decks && decks.map(deck => <div>{deck}</div>)}
+            </div>
+            <h1>Vite + React</h1>
+        </>
+    );
+}
+
+function App() {
+    return (
+        <QueryClientProvider client={new QueryClient()}>
+            <Home />
+        </QueryClientProvider>
+    )
 }
 
 export default App
