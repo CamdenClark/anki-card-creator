07bf5e34bc7ce1905571e1ec3e84cc266e61c284
Camden <camdenaws@gmail.com>
2023-10-14 13:23:48 -0700

    aider: Created a new React context for the OpenAI API key in `main.tsx`.
    # Aider chat conversation:
    
    USER: OK, now my goal is to make it so the openai key is configured on the settings page instead of the environment. I'm thinking that the best way to do that is by using a new react context specifically for the openai api key. can we start with that on main.tsx?
    
    ASSISTANT:
    ^C KeyboardInterrupt
    
    USER: OK, now my goal is to make it so the openai key is configured on the settings page instead of the environment. I'm thinking that the best way to do that is by using a new react context specifically for the openai api key. can we start with that on main.tsx?
    
    ASSISTANT: Sure, we can create a new React context for the OpenAI API key in the `src/main.tsx` file.
    
    Here are the steps:
    
    1. Import `createContext` and `useState` from `react`.
    2. Create a new context for the OpenAI API key.
    3. Create a state for the OpenAI API key and provide it to the context.
    
    Here is the *edit block* for these changes:
    
    ```tsx
    src/main.tsx
    <<<<<<< HEAD
    import React from 'react'
    import ReactDOM from 'react-dom/client'
    import App from './App.tsx'
    import { Outlet, RouterProvider, createBrowserRouter, Link } from 'react-router-dom'
    =======
    import React, { createContext, useState } from 'react'
    import ReactDOM from 'react-dom/client'
    import App from './App.tsx'
    import { Outlet, RouterProvider, createBrowserRouter, Link } from 'react-router-dom'
    
    export const OpenAIKeyContext = createContext();
    
    function Root() {
        const [openAIKey, setOpenAIKey] = useState('');
        return (
            <OpenAIKeyContext.Provider value={{ openAIKey, setOpenAIKey }}>
                <AppBar position="static">
                    <Toolbar>
                        <Typography variant="h6" component={Link} to="/" color="inherit" sx={{ textDecoration: 'none' }}>anki card creator</Typography>
                        <Typography sx={{ marginLeft: 4, textDecoration: 'none' }} component={Link} to="/settings" color="inherit">Settings</Typography>
                    </Toolbar>
                </AppBar>
                <Outlet />
            </OpenAIKeyContext.Provider>
        );
    }
    >>>>>>> updated
    ```
    
    This change will create a new context for the OpenAI API key and provide it to the entire application. The OpenAI API key can be updated using the `setOpenAIKey` function.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { createContext, useState } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import { Outlet, RouterProvider, createBrowserRouter, Link } from 'react-router-dom'
+
+export const OpenAIKeyContext = createContext();
+
+function Root() {
+    const [openAIKey, setOpenAIKey] = useState('');
+    return (
+        <OpenAIKeyContext.Provider value={{ openAIKey, setOpenAIKey }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" component={Link} to="/" color="inherit" sx={{ textDecoration: 'none' }}>anki card creator</Typography>
+                    <Typography sx={{ marginLeft: 4, textDecoration: 'none' }} component={Link} to="/settings" color="inherit">Settings</Typography>
+                </Toolbar>
+            </AppBar>
+            <Outlet />
+        </OpenAIKeyContext.Provider>
+    );
+}
 
 import Settings from './Settings.tsx';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
