883118c0a6fdd37325d6de0fdc6c999936850361
Camden <camdenaws@gmail.com>
2023-10-14 14:28:35 -0700

    Use gpt-4 by default...

--- src/openai.ts
+++ src/openai.ts
@@ -39,7 +39,7 @@ export async function suggestAnkiNotes(
         }
     }
     const body = {
-        model: 'gpt-3.5-turbo',
+        model: 'gpt-4',
         messages: [
             {
                 role: 'system',
