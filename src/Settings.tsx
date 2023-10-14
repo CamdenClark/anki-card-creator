962bcad5adea88aa1338cd4775e09d97aea0fc41
Camden <camdenaws@gmail.com>
2023-10-14 13:42:08 -0700

    aider: Added password type text field to control the `openAIKey` in local storage in `src/Settings.tsx`.
    # Aider chat conversation:
    
    USER: OK, now in `src/Settings.tsx` we should have a password type text field that will control the openaikey in local storage
    
    ASSISTANT: To add a password type text field that will control the `openAIKey` in local storage, we need to modify the `src/Settings.tsx` file. We will use the `useLocalStorage` hook to get and set the `openAIKey`. We will also import the `TextField` component from `@mui/material` to create the password field.
    
    Here are the changes:
    
    ```tsx
    src/Settings.tsx
    <<<<<<< HEAD
    import React from 'react';
    
    function Settings() {
        return (
            <div>
                <h1>Settings</h1>
            </div>
        );
    }
    
    export default Settings;
    =======
    import React from 'react';
    import { TextField } from '@mui/material';
    import useLocalStorage from './useLocalStorage';
    
    function Settings() {
        const [openAIKey, setOpenAIKey] = useLocalStorage('openAIKey', '');
    
        const handleOpenAIKeyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
            setOpenAIKey(event.target.value);
        };
    
        return (
            <div>
                <h1>Settings</h1>
                <TextField
                    type="password"
                    label="OpenAI Key"
                    value={openAIKey}
                    onChange={handleOpenAIKeyChange}
                />
            </div>
        );
    }
    
    export default Settings;
    >>>>>>> updated
    ```
    This change adds a password field to the Settings component. The value of this field is tied to the `openAIKey` in local storage. When the value of the field changes, the `openAIKey` in local storage is updated.

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { TextField } from '@mui/material';
+import useLocalStorage from './useLocalStorage';
 
 function Settings() {
+    const [openAIKey, setOpenAIKey] = useLocalStorage('openAIKey', '');
+
+    const handleOpenAIKeyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setOpenAIKey(event.target.value);
+    };
+
     return (
         <div>
             <h1>Settings</h1>
+            <TextField
+                type="password"
+                label="OpenAI Key"
+                value={openAIKey}
+                onChange={handleOpenAIKeyChange}
+            />
         </div>
     );
 }
