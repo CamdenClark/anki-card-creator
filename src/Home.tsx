b5a8cec2d4d9168d654f0fa174def8613e0431f5
Camden <camdenaws@gmail.com>
2023-10-20 21:59:22 -0700

    aider: Added a blurb at the top of the home page that says "Anki Card Creator is a tool that helps you create Anki cards quickly and easily using AI."
    # Aider chat conversation:
    
    USER: OK, now add a blurb at the top that has some info about what the goal of anki card creator is
    
    ASSISTANT: To add a blurb at the top of the home page, we need to modify the `src/Home.tsx` file. We will add a Typography component above the Card component to display the blurb.
    
    Here are the changes:
    
    ```tsx
    src/Home.tsx
    <<<<<<< HEAD
    import React from 'react';
    import { Card, CardActionArea, CardContent, Typography } from '@mui/material';
    import { useNavigate } from 'react-router-dom';
    
    function Home() {
        const navigate = useNavigate();
    
        return (
            <Card sx={{ maxWidth: 345, borderRadius: 2, margin: 'auto', marginTop: 5 }}>
                <CardActionArea onClick={() => navigate('/suggest')}>
                    <CardContent>
                        <Typography variant="h5" component="div" align="center">
                            Suggest cards with AI
                        </Typography>
                    </CardContent>
                </CardActionArea>
            </Card>
        );
    }
    
    export default Home;
    =======
    import React from 'react';
    import { Card, CardActionArea, CardContent, Typography } from '@mui/material';
    import { useNavigate } from 'react-router-dom';
    
    function Home() {
        const navigate = useNavigate();
    
        return (
            <div>
                <Typography variant="h6" component="p" align="center" sx={{ marginTop: 5 }}>
                    Anki Card Creator is a tool that helps you create Anki cards quickly and easily using AI.
                </Typography>
                <Card sx={{ maxWidth: 345, borderRadius: 2, margin: 'auto', marginTop: 5 }}>
                    <CardActionArea onClick={() => navigate('/suggest')}>
                        <CardContent>
                            <Typography variant="h5" component="div" align="center">
                                Suggest cards with AI
                            </Typography>
                        </CardContent>
                    </CardActionArea>
                </Card>
            </div>
        );
    }
    
    export default Home;
    >>>>>>> updated
    ```
    
    This will add a blurb at the top of the home page that says "Anki Card Creator is a tool that helps you create Anki cards quickly and easily using AI." The blurb is centered and has some margin at the top.

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,15 +6,20 @@ function Home() {
     const navigate = useNavigate();
 
     return (
-        <Card sx={{ maxWidth: 345, borderRadius: 2, margin: 'auto', marginTop: 5 }}>
-            <CardActionArea onClick={() => navigate('/suggest')}>
-                <CardContent>
-                    <Typography variant="h5" component="div" align="center">
-                        Suggest cards with AI
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
+        <div>
+            <Typography variant="h6" component="p" align="center" sx={{ marginTop: 5 }}>
+                Anki Card Creator is a tool that helps you create Anki cards quickly and easily using AI.
+            </Typography>
+            <Card sx={{ maxWidth: 345, borderRadius: 2, margin: 'auto', marginTop: 5 }}>
+                <CardActionArea onClick={() => navigate('/suggest')}>
+                    <CardContent>
+                        <Typography variant="h5" component="div" align="center">
+                            Suggest cards with AI
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </div>
     );
 }
 
